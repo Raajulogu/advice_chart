ece9768869579ba3db9b7a4ea5f32656895c7a9a
Raajulogu <111479920+Raajulogu@users.noreply.github.com>
2023-04-06 11:52:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,10 +28,3 @@ async function advice() {
         content.innerHTML="Please check Your Internet connection"
     }
 }
-
-
-//wQiWXohWQv1LeOazA6KdwRnUWxFsdNPf
-//Kickbox
-//Email verification https://api.kickbox.com/v2/verify?email=145@l.com&apikey=test_f2c0e45fe39ccb0abbf973da45c3760ffe55f4901a7bb30072fca0d05dedd730
-//https://api.adviceslip.com/advice
-
